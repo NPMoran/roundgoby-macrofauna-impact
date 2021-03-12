0ca70f52359992e0952b849763c2f8324824676a
Nicholas Patrick Moran <npamo@dtu.dk>
2021-03-12 11:26:00 +0100

    ...

--- 3_visualisation.R
+++ 3_visualisation.R
@@ -10,7 +10,7 @@
 
 Sys.setenv(LANG = "en")
 
-library(dplyr); library(ggplot2); library(RColorBrewer)
+library(dplyr); library(rlang); library(tidyverse); library(ggplot2); library(labeling)
 
 
 load("./models/GULD.brms.origtaxa.negbinom.RData")
@@ -19,9 +19,40 @@ load("./models/GULD.brms.fulltaxa.negbinom.RData")
 load("./models/STBT.brms.fulltaxa.negbinom.RData")
 
 
-working <- as.data.frame(coef(STBT.brms.origtaxa.negbinom, probs = c(0.05, 0.95)))
-
-
 #Figure 1a. Guldborgsund
+working <- as.data.frame(coef(GULD.brms.origtaxa.negbinom, probs = c(0.05, 0.95)))
+
+data_GULDfig <- NULL
+data_GULDfig$estimate <- working$Artsgruppering.Estimate.BAbAfter
+data_GULDfig$lower <- working$Artsgruppering.Q5.BAbAfter
+data_GULDfig$upper <- working$Artsgruppering.Q95.BAbAfter
+data_GULDfig$positions <- c( 3, 8, 5, 4, 7, 1, 6, 2)
+data_GULDfig$color <- c( "darkorchid4", "deeppink4", "darkmagenta", "darkmagenta", "deeppink4", "darkorchid1", "darkmagenta", "darkorchid1")
+data_GULDfig$labels <- rownames(working)
+data_GULDfig <- as.data.frame(data_GULDfig)
+
+GULDfig <- ggplot(data_GULDfig, aes(x = estimate, y = positions)) +
+  theme(axis.text.y = element_blank(), 
+        axis.ticks.y = element_blank(), 
+        axis.text.x = element_text(size = 8.5, colour = "black"), 
+        panel.background = element_rect(fill = "white"),
+        axis.title.x  = element_text(size=13, vjust = 0.1),
+        panel.border = element_rect(colour = "black", fill=NA, size = 1)) +
+  geom_segment(x = data_GULDfig$lower, y = data_GULDfig$pos, xend = data_GULDfig$upper, yend = data_GULDfig$pos, size = 1, colour = data_GULDfig$color) + 
+  geom_point(shape = 19, size = 2, colour = data_GULDfig$color) +
+  geom_vline(xintercept = 0, linetype = 2, colour = "black", size = 0.5) +
+  geom_text(aes(label=labels, fontface = 4), hjust = "left", x =-8, vjust=-1.5, size = 2.5) +
+  labs(x = "Before-after impact effect",
+     y = "") +
+  xlim(-7.5, 15) +
+  ylim(0.5, 8.5)
+GULDfig
+
+ggsave("GULDfig.jpg", width = 18, height = 8, units = "cm", GULDfig, dpi = 600)
 
 
+
+
+
+#Figure 1b. Stege Bugt
+working <- as.data.frame(coef(STBT.brms.origtaxa.negbinom, probs = c(0.05, 0.95)))
