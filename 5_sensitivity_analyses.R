478a42942cb472cdf654e555b8bd3608082cc536
Nicholas Moran <nicholaspatrickmoran@gmail.com>
2021-04-30 17:29:44 +0200

    ...

--- 5_sensitivity_analyses.R
+++ 5_sensitivity_analyses.R
@@ -7,6 +7,8 @@
 
 
 #### 6. Sensitivity Analysis -
+# NOTE: Sensitivity analysis run unsing Guldborgsund dataset, as our main Results (gut content analysis) are based on this site
+
 # Part 1: rerunning analysis zero inflated neg binomial distribution ####
 
 Sys.setenv(LANG = "en")
@@ -40,165 +42,58 @@ ranef(GULD.brms.fulltaxa.zerinfnb)
 fixef(GULD.brms.fulltaxa.zerinfnb)
 r2_bayes(GULD.brms.fulltaxa.zerinfnb, ci = 0.95)
 
-save(GULD.brms.fulltaxa.zerinfnb, file = "./models/A1_sensitivity/GULD.brms.fulltaxa.zerinfnb.RData")
+save(GULD.brms.fulltaxa.zerinfnb, file = "./models/sensitivity/GULD.brms.fulltaxa.zerinfnb.RData")
 
-
-STBT.brms.fulltaxa.zerinfnb <- brm(Count ~
-                                     1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Year), data=data_STBTgrouped, 
-                                   family = zero_inflated_negbinomial(),
-                                   control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                   chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(STBT.brms.fulltaxa.zerinfnb)
-summary(STBT.brms.fulltaxa.zerinfnb)
-ranef(STBT.brms.fulltaxa.zerinfnb)
-fixef(STBT.brms.fulltaxa.zerinfnb)
-r2_bayes(STBT.brms.fulltaxa.zerinfnb, ci = 0.95)
-
-save(STBT.brms.fulltaxa.zerinfnb, file = "./models/A1_sensitivity/STBT.brms.fulltaxa.zerinfnb.RData")
 
 
 # Part 2: Including sampling month as an additional random effect ####
-# Sampling in 2015 was conducted approx 2 months earlier than in previous seasons,
-# Therefore, checking if similar responses are detected excluding 2015, or by including sampling month as a factor
+# Sampling in 2015 was conducted approx 2 months earlier than in previous seasons, therefore, checking if similar responses are detected excluding 2015
 
-#Including sampling month as a random effect
-data_GULDgrouped$Month <- if_else(data_GULDgrouped$Year == '2015', 'March', 'May')
-data_STBTgrouped$Month <- if_else(data_STBTgrouped$Year == '2015', 'March', 'May')
-
-GULD.brms.fulltaxa.negbinomA2 <- brm(Count ~
-                                       1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Month) + (1|Year) , data=data_GULDgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(GULD.brms.fulltaxa.negbinomA2)
-summary(GULD.brms.fulltaxa.negbinomA2)
-ranef(GULD.brms.fulltaxa.negbinomA2)
-fixef(GULD.brms.fulltaxa.negbinomA2)
-r2_bayes(GULD.brms.fulltaxa.negbinomA2, ci = 0.95)
-
-save(GULD.brms.fulltaxa.negbinomA2, file = "./models/A2_sensitivity/GULD.brms.fulltaxa.negbinomA2.RData")
-
-
-STBT.brms.fulltaxa.negbinomA2 <- brm(Count ~
-                                       1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Month) + (1|Year), data=data_STBTgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(STBT.brms.fulltaxa.negbinomA2)
-summary(STBT.brms.fulltaxa.negbinomA2)
-ranef(STBT.brms.fulltaxa.negbinomA2)
-fixef(STBT.brms.fulltaxa.negbinomA2)
-r2_bayes(STBT.brms.fulltaxa.negbinomA2, ci = 0.95)
-
-save(STBT.brms.fulltaxa.negbinomA2, file = "./models/A2_sensitivity/STBT.brms.fulltaxa.negbinomA2.RData")
-
-
-#Using Month as a fixed effect
-GULD.brms.fulltaxa.negbinomA3 <- brm(Count ~
-                                       1 + BA + Month + (1 + BA + Month|TaxaGroup) + (1|SampleID) + (1|Year) , data=data_GULDgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(GULD.brms.fulltaxa.negbinomA3)
-summary(GULD.brms.fulltaxa.negbinomA3)
-ranef(GULD.brms.fulltaxa.negbinomA3)
-fixef(GULD.brms.fulltaxa.negbinomA3)
-r2_bayes(GULD.brms.fulltaxa.negbinomA3, ci = 0.95)
-
-save(GULD.brms.fulltaxa.negbinomA3, file = "./models/A2_sensitivity/GULD.brms.fulltaxa.negbinomA3.RData")
-
-
-STBT.brms.fulltaxa.negbinomA3 <- brm(Count ~
-                                       1 + BA + Month + (1 + BA + Month|TaxaGroup) + (1|SampleID) + (1|Year), data=data_STBTgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(STBT.brms.fulltaxa.negbinomA3)
-summary(STBT.brms.fulltaxa.negbinomA3)
-ranef(STBT.brms.fulltaxa.negbinomA3)
-fixef(STBT.brms.fulltaxa.negbinomA3)
-r2_bayes(STBT.brms.fulltaxa.negbinomA3, ci = 0.95)
-
-save(STBT.brms.fulltaxa.negbinomA3, file = "./models/A2_sensitivity/STBT.brms.fulltaxa.negbinomA3.RData")
-
-
-
-#Alternate way of doing the above, using a taxa-specific month random effect.
-data_GULDgrouped$Month2 <- paste(data_GULDgrouped$Month, data_GULDgrouped$TaxaGroup, sep = '')
-data_STBTgrouped$Month2 <- paste(data_STBTgrouped$Month, data_STBTgrouped$TaxaGroup, sep = '')
-
-
-GULD.brms.fulltaxa.negbinomA4 <- brm(Count ~
-                                       1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Year) + (1|Month2), data=data_GULDgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(GULD.brms.fulltaxa.negbinomA4)
-summary(GULD.brms.fulltaxa.negbinomA4)
-ranef(GULD.brms.fulltaxa.negbinomA4)
-fixef(GULD.brms.fulltaxa.negbinomA4)
-r2_bayes(GULD.brms.fulltaxa.negbinomA4, ci = 0.95)
-
-save(GULD.brms.fulltaxa.negbinomA4, file = "./models/A2_sensitivity/GULD.brms.fulltaxa.negbinomA4.RData")
-
-
-STBT.brms.fulltaxa.negbinomA4 <- brm(Count ~
-                                       1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Year) + (1|Month2), data=data_STBTgrouped, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
-#plot(STBT.brms.fulltaxa.negbinomA4)
-summary(STBT.brms.fulltaxa.negbinomA4)
-ranef(STBT.brms.fulltaxa.negbinomA4)
-fixef(STBT.brms.fulltaxa.negbinomA4)
-r2_bayes(STBT.brms.fulltaxa.negbinomA4, ci = 0.95)
-
-save(STBT.brms.fulltaxa.negbinomA4, file = "./models/A2_sensitivity/STBT.brms.fulltaxa.negbinomA4.RData")
-
-
-#Models just deleting 2015
+#First, testing for taxa-specific responses are influenced by excluding 2015
 data_GULDgrouped2015 <- subset(data_GULDgrouped, Year != 2015)
-data_STBTgrouped2015 <- subset(data_STBTgrouped, Year != 2015)
-GULD.brms.fulltaxa.negbinomA5 <- brm(Count ~
+
+GULD.brms.fulltaxa.negbinom2015 <- brm(Count ~
                                        1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Year), data=data_GULDgrouped2015, 
                                      family = negbinomial(),
                                      control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = 500, warmup = 200, thin = thinning)
-#plot(GULD.brms.fulltaxa.negbinomA5)
-summary(GULD.brms.fulltaxa.negbinomA5)
-ranef(GULD.brms.fulltaxa.negbinomA5)
-fixef(GULD.brms.fulltaxa.negbinomA5)
-r2_bayes(GULD.brms.fulltaxa.negbinomA5, ci = 0.95)
+                                     chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
+#plot(GULD.brms.fulltaxa.negbinom2015)
+summary(GULD.brms.fulltaxa.negbinom2015)
+ranef(GULD.brms.fulltaxa.negbinom2015)
+fixef(GULD.brms.fulltaxa.negbinom2015)
+r2_bayes(GULD.brms.fulltaxa.negbinom2015, ci = 0.95)
 
-save(GULD.brms.fulltaxa.negbinomA5, file = "./models/A2_sensitivity/GULD.brms.fulltaxa.negbinomA5.RData")
-
-
-STBT.brms.fulltaxa.negbinomA5 <- brm(Count ~
-                                       1 + BA + (1 + BA|TaxaGroup) + (1|SampleID) + (1|Year), data=data_STBTgrouped2015, 
-                                     family = negbinomial(),
-                                     control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
-                                     chains = 2, cores = 4, iter = 500, warmup = 200, thin = thinning)
-#plot(STBT.brms.fulltaxa.negbinomA5)
-summary(STBT.brms.fulltaxa.negbinomA5)
-ranef(STBT.brms.fulltaxa.negbinomA5)
-fixef(STBT.brms.fulltaxa.negbinomA5)
-r2_bayes(STBT.brms.fulltaxa.negbinomA5, ci = 0.95)
-
-save(STBT.brms.fulltaxa.negbinomA5, file = "./models/A2_sensitivity/STBT.brms.fulltaxa.negbinomA5.RData")
+save(GULD.brms.fulltaxa.negbinom2015, file = "./models/sensitivity/GULD.brms.fulltaxa.negbinom2015.RData")
 
 
+#Second, testing if gut content precense-absence hypothesis testing model is influenced by excluding 2015
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Littorinimorpha (small)', 'Present', 'Absent')
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Littorinimorpha (large)', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Neritidae', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Cardiidae', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Isopoda', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Amphipoda', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
+data_GULDgrouped$Presence <- if_else(data_GULDgrouped$TaxaGroup == 'Myidae', 'Present', data_GULDgrouped$Presence <- data_GULDgrouped$Presence)
 
+data_GULDgrouped2015 <- subset(data_GULDgrouped, Year != 2015)
 
+GULD.brms.guttest.negbinom_PresenceTEST2015 <- brm(Count ~
+                                                 1 + BA*Presence + (BA|TaxaGroup) + (1|SampleID) + (1|Year), data=data_GULDgrouped2015, 
+                                               family = negbinomial(),
+                                               control = list(adapt_delta = adapt_delta_value, max_treedepth = max_treedepth_value),
+                                               chains = 2, cores = 4, iter = iterations, warmup = burnin, thin = thinning)
+#plot(GULD.brms.guttest.negbinom_PresenceTEST2015)
+summary(GULD.brms.guttest.negbinom_PresenceTEST2015)
+fixef(GULD.brms.guttest.negbinom_PresenceTEST2015)
+r2_bayes(GULD.brms.guttest.negbinom_PresenceTEST2015, ci = 0.95)
+
+save(GULD.brms.guttest.negbinom_PresenceTEST2015, file = "./models/sensitivity/GULD.brms.guttest.negbinom_PresenceTEST2015.RData")
 
 
 # Plotting comparison (Figure S2)
 load("./models/GULD.brms.fulltaxa.negbinom.RData")
-load("./models/STBT.brms.fulltaxa.negbinom.RData")
-load("./models/A1_sensitivity/GULD.brms.fulltaxa.zerinfnb.RData")
-load("./models/A1_sensitivity/STBT.brms.fulltaxa.zerinfnb.RData")
-load("./models/A2_sensitivity/GULD.brms.fulltaxa.negbinomA5.RData")
-load("./models/A2_sensitivity/STBT.brms.fulltaxa.negbinomA4.RData")
-
+load("./models/sensitivity/GULD.brms.fulltaxa.zerinfnb.RData")
+load("./models/sensitivity/GULD.brms.fulltaxa.negbinom2015.RData")
 
 working1 <- as.data.frame(ranef(GULD.brms.fulltaxa.negbinom, groups = 'TaxaGroup'))
 working2 <- as.data.frame(ranef(GULD.brms.fulltaxa.zerinfnb, groups = 'TaxaGroup'))
@@ -252,61 +147,5 @@ SUPPfigA <- ggplot(data_SUPPfigA, aes(x = estimate1, y = positions)) +
        y = "") 
 SUPPfigA
 
-ggsave("./visualisations/SUPPfigAxx.jpg", width = 8, height = 16, units = "cm", SUPPfigA, dpi = 600)
+ggsave("./visualisations/SUPPfig.jpg", width = 8, height = 16, units = "cm", SUPPfigA, dpi = 600)
 
-
-
-
-working1 <- as.data.frame(ranef(STBT.brms.fulltaxa.negbinom, groups = 'TaxaGroup'))
-working2 <- as.data.frame(ranef(STBT.brms.fulltaxa.zerinfnb, groups = 'TaxaGroup'))
-working3 <- as.data.frame(ranef(STBT.brms.fulltaxa.negbinomA4, groups = 'TaxaGroup'))
-
-#Building dataframe
-data_SUPPfigB <- NULL
-data_SUPPfigB$estimate1 <- working1$TaxaGroup.Estimate.BAbAfter
-data_SUPPfigB$lower1 <- working1$TaxaGroup.Q2.5.BAbAfter
-data_SUPPfigB$upper1 <- working1$TaxaGroup.Q97.5.BAbAfter
-data_SUPPfigB$estimate2 <- working2$TaxaGroup.Estimate.BAbAfter
-data_SUPPfigB$lower2 <- working2$TaxaGroup.Q2.5.BAbAfter
-data_SUPPfigB$upper2 <- working2$TaxaGroup.Q97.5.BAbAfter
-data_SUPPfigB$estimate3 <- working3$TaxaGroup.Estimate.BAbAfter
-data_SUPPfigB$lower3 <- working3$TaxaGroup.Q2.5.BAbAfter
-data_SUPPfigB$upper3 <- working3$TaxaGroup.Q97.5.BAbAfter
-
-data_SUPPfigB$positions <- c(14.5, 1.0, 9.0, 23.5, 5.5, 
-                            6.5, 13.5, 18.0, 17.0, 16.0, 
-                            21.5, 22.5, 4.0, 11.0, 19.0, 
-                            10.0, 12.0, 20.5, 2.5)
-data_SUPPfigB$position1 <- data_SUPPfigB$positions + 0.45
-data_SUPPfigB$position2 <- data_SUPPfigB$positions + 0.2
-data_SUPPfigB$position3 <- data_SUPPfigB$positions - 0.05
-
-data_SUPPfigB$labels <- rownames(working1)
-data_SUPPfigB <- as.data.frame(data_SUPPfigB)
-
-
-SUPPfigB <- ggplot(data_SUPPfigB, aes(x = estimate1, y = positions)) +
-  scale_x_continuous(limits = c(-11, 7), expand = c(0, 0), breaks=c(-5.0,-2.5,0.0,2.5,5.0)) +
-  scale_y_continuous(limits = c(0.25, 25.5), expand = c(0, 0), breaks=NULL) +
-  theme(legend.position = "none",
-        axis.text.y = element_blank(), 
-        axis.ticks.y = element_blank(), 
-        axis.text.x = element_text(size = 5, colour = "black"), 
-        axis.line.x = element_line(colour = "black", size = 0.5),
-        panel.background = element_rect(fill = "white"),
-        axis.title.x  = element_text(size=8, vjust = 0.1),
-        panel.border = element_rect(colour = "black", fill=NA, size = 1)) +
-  geom_segment(x = data_SUPPfigB$lower1, y = data_SUPPfigB$position1, xend = data_SUPPfigB$upper1, yend = data_SUPPfigB$position1, size = 0.4) + 
-  geom_point(x = data_SUPPfigB$estimate1, y = data_SUPPfigB$position1, shape = 19, size = 0.8) +
-  geom_segment(colour = "dodgerblue2", x = data_SUPPfigB$lower2, y = data_SUPPfigB$position2, xend = data_SUPPfigB$upper2, yend = data_SUPPfigB$position2, size = 0.4) + 
-  geom_point(colour = "dodgerblue2", x = data_SUPPfigB$estimate2, y = data_SUPPfigB$position2, shape = 19, size = 0.8) +
-  geom_segment(colour = "firebrick2", x = data_SUPPfigB$lower3, y = data_SUPPfigB$position3, xend = data_SUPPfigB$upper3, yend = data_SUPPfigB$position3, size = 0.4) + 
-  geom_point(colour = "firebrick2", x = data_SUPPfigB$estimate3, y = data_SUPPfigB$position3, shape = 19, size = 0.8) +
-  geom_vline(xintercept = 0, linetype = 2, colour = "black", size = 0.5) +
-  geom_hline(yintercept = c(2, 3.5, 5.0, 7.5, 13.0, 15.5, 20.0), linetype = 3, colour = "black", size = 0.25) +
-  geom_text(aes(label=labels, fontface = 3), hjust = "left", x =-10.5, vjust=-1.2, size = 1.8) +
-  labs(x = "Before-after impact effect (Stege Bugt)",
-       y = "") 
-SUPPfigB
-
-ggsave("./visualisations/SUPPfigB.jpg", width = 8, height = 16, units = "cm", SUPPfigB, dpi = 600)
